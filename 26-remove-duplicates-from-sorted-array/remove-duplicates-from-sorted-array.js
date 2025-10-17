a80fce40f41698e65d14537d97bb42051b365624
Aslah_c <aslah.c7070@gmail.com>
2025-10-17 20:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 58.3 MB (38.37%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-      let k=0
-  for(let i=1;i<nums.length;i++){
-            if(nums[k]!==nums[i]){
-                k++
-                nums[k]=nums[i]
-            }
-  }
- return k+1
+    let i=0;
+for(let j=1;j<nums.length;j++){
+    if(nums[j]!==nums[i]){
+     i++
+     nums[i]=nums[j]
+    }
+}
+return i+1
+    
 };
