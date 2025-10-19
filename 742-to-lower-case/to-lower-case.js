4a95607400ffa2554088b4c949d65c37a8aabda4
Aslah_c <aslah.c7070@gmail.com>
2025-10-19 10:46:55 +0530

    Time: 14 ms (0.38%) | Memory: 56.6 MB (5.53%) - LeetSync

--- 742-to-lower-case/to-lower-case.js
+++ 742-to-lower-case/to-lower-case.js
@@ -4,5 +4,21 @@
  */
 var toLowerCase = function(s) {
     
-    return s.toLowerCase()
+    // return s.toLowerCase()
+
+    let str="";
+    for(let x of s){
+        
+        let code=x.charCodeAt()
+        if(code>=65&&code<=90){
+            console.log(x+"-"+code)
+            str+=String.fromCharCode(code+32)
+        }else{
+            str+=x
+        }
+
+
+    }
+
+    return str
 };
