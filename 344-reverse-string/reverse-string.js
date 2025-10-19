97166c1c2358619630d5a2ca44ed16b0aa328b27
Aslah_c <aslah.c7070@gmail.com>
2025-10-19 10:32:42 +0530

    Time: 3 ms (13.34%) | Memory: 59.9 MB (82.62%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -4,6 +4,16 @@
  */
 var reverseString = function(s) {
     
-    s.reverse()
+    // s.reverse()
+    // return s
+
+
+    let left=0
+    let right=s.length-1
+    while(left<right){
+        [s[left],s[right]]=[s[right],s[left]]
+        left++
+        right--
+    }
     return s
 };
